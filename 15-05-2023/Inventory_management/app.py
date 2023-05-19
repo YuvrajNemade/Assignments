da3368c2e20c3e7cf23cf9511e4f0c77b9737230
YuvrajNemade <104988855+YuvrajNemade@users.noreply.github.com>
2023-05-19 18:54:57 +0530

    Delete 15-05-2023/Inventory_management directory

--- 15-05-2023/Inventory_management/app.py
+++
@@ -1,5 +0,0 @@
-import uvicorn
-
-if __name__ == "__main__":
-    while True:
-        uvicorn.run("main:app", reload=True)
--- 15-05-2023/Inventory_management/scripts/constant/email_constant.py
+++
@@ -1,6 +0,0 @@
-class Email_Constants:
-    email_name = "yuvrajnemade21@gmail.com"
-    email_password = "jkpaaeoxbkcfzsxt"
-
-
-email_object = Email_Constants()
